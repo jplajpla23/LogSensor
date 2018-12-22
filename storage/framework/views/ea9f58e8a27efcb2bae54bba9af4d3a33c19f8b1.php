b47be16055a26d87c537750bc70d8cff92ccc95f
jplajpla23 <jplajpla23@gmail.com>
2018-12-22 18:25:01 +0000

    DashBoard Initial

--- storage/framework/views/ea9f58e8a27efcb2bae54bba9af4d3a33c19f8b1.php
+++ storage/framework/views/ea9f58e8a27efcb2bae54bba9af4d3a33c19f8b1.php
@@ -1,25 +1,27 @@
 <?php $__env->startSection('content'); ?>
 <div class="container">
+    <span id="error" style="display: none!important;"><?php echo e($errors->any()); ?></span>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header"><center><h4>DashBoard</h4></center></div><br>
+                
                 <div class="card-body">
-                    <?php if(session('status')): ?>
-                        <div class="alert alert-success" role="alert">
-                            <?php echo e(session('status')); ?>
 
-                        </div>
-                    <?php endif; ?>
-                   <!--<center>
-                    <a class="btn btn-outline-danger" href="<?php echo e(route('listSensores')); ?>"> <i class="fas fa-wifi"></i> Sensors</a>
-                    <a class="btn btn-outline-info" href="<?php echo e(route('listAlerts')); ?>"> <i class="fas fa-bell"></i> Alerts</a></center>-->
+
+
+
                 </div>
+                
+                
+
+
             </div>
         </div>
     </div>
 </div>
+</div>
+
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
