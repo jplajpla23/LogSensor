a5e4b9957d3c4c35714d4234644a027a2a3d0b2b
jplajpla23 <jplajpla23@gmail.com>
2018-12-23 14:26:38 +0000

    undefined variables fix dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,25 @@
 @section('content')
 
 <div class="container">
-    <span id="error" style="display: none!important;">{{$errors->any()}}</span>
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header"><center><h4>DashBoard</h4></center></div><br>
-                
-                <div class="card-body">
-                    <center>
-                        <div id="chart_div" style="width: 100%!important;" ></div>
-                    </center>
+  <span id="error" style="display: none!important;">{{$errors->any()}}</span>
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header"><center><h4>DashBoard</h4></center></div><br>
 
-                </div>
-                
-                
+        <div class="card-body">
+          <center>
+            <div id="chart_div" style="width: 100%!important;" ></div>
+          </center>
 
-
-            </div>
         </div>
+
+
+
+
+      </div>
     </div>
+  </div>
 </div>
 </div>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -29,48 +29,52 @@
   google.charts.setOnLoadCallback(drawChart);
   function drawChart() {
 
-        var data = new google.visualization.DataTable();
-      data.addColumn('datetime', 'X');
-      @foreach ($historynames as $h)
-      data.addColumn('number', '{{ $h }}');
+    
+    @isset($historynames)
+    var data = new google.visualization.DataTable();
+    data.addColumn('datetime', 'X');
+    @foreach ($historynames as $h)
+    data.addColumn('number', '{{ $h }}');
+    @endforeach
+    @endisset
+    data.addRows([
+      @isset($history)
+      @foreach ($history as $ha)
+      @foreach ($ha as $h)
+      {{-- expr --}}
+
+      [new Date('{{ $h->created_at }}'), 
+      @foreach ($historynames as $key=>$hn) 
+      @if($key== $h->sensors_id)
+      {{ $h->value }},
+      @else
+      @if (!$loop->last)
+      {{ 'null,' }}
+      @else
+      {{ 'null' }}
+      @endif
+
+      @endif
       @endforeach
+      {{-- expr --}}
+      ],
+      @endforeach
+      @endforeach 
+        @endisset       
+      ]);
+    
+    var options = {
+      hAxis: {
+        title: 'Time'
+      },
+      vAxis: {
+        title: 'Values'
+      }
+    };
 
-      data.addRows([
-        @foreach ($history as $ha)
-        @foreach ($ha as $h)
-        {{-- expr --}}
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 
-        [new Date('{{ $h->created_at }}'), 
-        @foreach ($historynames as $key=>$hn) 
-        @if($key== $h->sensors_id)
-        {{ $h->value }},
-        @else
-        @if (!$loop->last)
-        {{ 'null,' }}
-        @else
-        {{ 'null' }}
-        @endif
-
-        @endif
-        @endforeach
-        {{-- expr --}}
-        ],
-        @endforeach
-        @endforeach        
-        ]);
-
-      var options = {
-        hAxis: {
-          title: 'Time'
-        },
-        vAxis: {
-          title: 'Values'
-        }
-      };
-
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-}
+    chart.draw(data, options);
+  }
 </script>
 @endsection
--- storage/framework/views/ea9f58e8a27efcb2bae54bba9af4d3a33c19f8b1.php
+++ storage/framework/views/ea9f58e8a27efcb2bae54bba9af4d3a33c19f8b1.php
@@ -1,25 +1,25 @@
 <?php $__env->startSection('content'); ?>
 
 <div class="container">
-    <span id="error" style="display: none!important;"><?php echo e($errors->any()); ?></span>
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header"><center><h4>DashBoard</h4></center></div><br>
-                
-                <div class="card-body">
-                    <center>
-                        <div id="chart_div" style="width: 100%!important;" ></div>
-                    </center>
+  <span id="error" style="display: none!important;"><?php echo e($errors->any()); ?></span>
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header"><center><h4>DashBoard</h4></center></div><br>
 
-                </div>
-                
-                
+        <div class="card-body">
+          <center>
+            <div id="chart_div" style="width: 100%!important;" ></div>
+          </center>
 
-
-            </div>
         </div>
+
+
+
+
+      </div>
     </div>
+  </div>
 </div>
 </div>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -28,51 +28,55 @@
   google.charts.setOnLoadCallback(drawChart);
   function drawChart() {
 
-        var data = new google.visualization.DataTable();
-      data.addColumn('datetime', 'X');
-      <?php $__currentLoopData = $historynames; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $h): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-      data.addColumn('number', '<?php echo e($h); ?>');
+    
+    <?php if(isset($historynames)): ?>
+    var data = new google.visualization.DataTable();
+    data.addColumn('datetime', 'X');
+    <?php $__currentLoopData = $historynames; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $h): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    data.addColumn('number', '<?php echo e($h); ?>');
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    <?php endif; ?>
+    data.addRows([
+      <?php if(isset($history)): ?>
+      <?php $__currentLoopData = $history; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ha): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+      <?php $__currentLoopData = $ha; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $h): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+      
+
+      [new Date('<?php echo e($h->created_at); ?>'), 
+      <?php $__currentLoopData = $historynames; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key=>$hn): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?> 
+      <?php if($key== $h->sensors_id): ?>
+      <?php echo e($h->value); ?>,
+      <?php else: ?>
+      <?php if(!$loop->last): ?>
+      <?php echo e('null,'); ?>
+
+      <?php else: ?>
+      <?php echo e('null'); ?>
+
+      <?php endif; ?>
+
+      <?php endif; ?>
       <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+      
+      ],
+      <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+      <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?> 
+        <?php endif; ?>       
+      ]);
+    
+    var options = {
+      hAxis: {
+        title: 'Time'
+      },
+      vAxis: {
+        title: 'Values'
+      }
+    };
 
-      data.addRows([
-        <?php $__currentLoopData = $history; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ha): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <?php $__currentLoopData = $ha; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $h): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 
-        [new Date('<?php echo e($h->created_at); ?>'), 
-        <?php $__currentLoopData = $historynames; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key=>$hn): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?> 
-        <?php if($key== $h->sensors_id): ?>
-        <?php echo e($h->value); ?>,
-        <?php else: ?>
-        <?php if(!$loop->last): ?>
-        <?php echo e('null,'); ?>
-
-        <?php else: ?>
-        <?php echo e('null'); ?>
-
-        <?php endif; ?>
-
-        <?php endif; ?>
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        
-        ],
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>        
-        ]);
-
-      var options = {
-        hAxis: {
-          title: 'Time'
-        },
-        vAxis: {
-          title: 'Values'
-        }
-      };
-
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-}
+    chart.draw(data, options);
+  }
 </script>
 <?php $__env->stopSection(); ?>
 
